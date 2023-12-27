6e857f9475ff85af371c9ee865e7e8f78e08b2b5
Kaushalya193 <e19193@eng.pdn.ac.lk>
2023-12-28 01:22:07 +0530

    login page and signup page

---
+++ code/Software/Frontend/src/Login_Page/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+
+import './Login.css'
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Perform login logic here (e.g., send data to an API)
+    console.log('Login submitted:', { username, password, rememberMe });
+    history.push('/dashboard');
+    
+    setUsername('');
+    setPassword('');
+    setRememberMe(false);
+  };
+
+  return ( 
+    <div className="login-container">
+      <div className="header-bar ">
+        <h2 style={{ color: 'white', textAlign: 'left' }}><b>SMART</b> <span className="thin-text">BILLING SYSTEM</span></h2>
+      </div>
+    <div className='form'>
+      <h2>Welcome back!</h2>
+      <form onSubmit={handleLogin}>
+      <label htmlFor="email">Username</label>
+      <div className="input-with-icon">
+        <FontAwesomeIcon icon={faUser} className="icon" />
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          required
+        />
+      </div>
+
+      <label htmlFor="email">Password</label>
+      <div className="input-with-icon">
+  <FontAwesomeIcon icon={faLock} className="icon" />
+  <input
+    type="password"
+    id="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    placeholder="strong password"
+    required
+  />
+</div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+
+        <div className="remember-me">
+        <label><input type="checkbox" id="reme" name="reme" />
+            Remember Me
+        </label>
+          </div>
+
+      </form>
+      <p>
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </div>
+    <div className="footer-bar ">
+        <h6 style={{ color: 'white', textAlign: 'center'}}><span className="thin-text">@2023 Smart Billing System. All right reserved.</span></h6>
+      </div>
+   </div>
+  );
+};
+
+export default Login;
---
+++ code/Software/Frontend/src/signin_Page/Signup.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import './signup.css';
+
+const Signup = () => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordMatch, setIsPasswordMatch] = useState(true);
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(email);
+
+    if (!isValidEmail) {
+      setIsEmailValid(false);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setIsPasswordMatch(false);
+      return;
+    }
+
+    console.log('Sign-up submitted:', {
+      fullName,
+      email,
+      username,
+      password,
+    });
+
+    setFullName('');
+    setEmail('');
+    setUsername('');
+    setPassword('');
+    setConfirmPassword('');
+    setIsEmailValid(true);
+    setIsPasswordMatch(true);
+  };
+
+  return (
+
+    <div className="signin-container">
+      <div className="header-bar ">
+        <h2 style={{ color: 'white', textAlign: 'left' }}><b>SMART</b> <span className="thin-text">BILLING SYSTEM</span></h2>
+      </div>
+    <div className='form-container'>
+        <div className='image-container'></div>
+    <div className='formsignin'>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignUp}>
+        <div>
+          <label htmlFor="fullName">Full Name:</label>
+          <input
+            type="text"
+            id="fullName"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setIsEmailValid(true);
+            }}
+            required
+          />
+          {!isEmailValid && <p className="error-text">Please enter a valid email address</p>}
+        </div>
+      
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+              setIsPasswordMatch(true);
+            }}
+            required
+          />
+          {!isPasswordMatch && <p className="error-text">Passwords do not match</p>}
+        </div>
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
+      </form>
+    </div>
+    </div>
+
+    <div className="footer-bar ">
+        <h6 style={{ color: 'white', textAlign: 'center'}}><span className="thin-text">@2023 Smart Billing System. All right reserved.</span></h6>
+      </div>
+
+    </div>
+  );
+};
+
+export default Signup;
